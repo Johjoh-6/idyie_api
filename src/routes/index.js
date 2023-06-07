51472bbd1e99f08c8274cb6573d09c04a36c0032
Johann six <six.johann@gmail.com>
2023-06-07 11:33:48 +0200

    🎨 update route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 module.exports = async function (fastify) {
 	fastify.register(require("./users"));
 	fastify.register(require("./auth"));
+	fastify.register(require("./categorie"));
+	fastify.register(require("./tutorial"));
 	// Add other route files here as needed
 };
