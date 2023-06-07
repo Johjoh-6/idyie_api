98a09116ba8c386a912466619b444acf4096fc8d
Johann six <six.johann@gmail.com>
2023-06-07 16:26:26 +0200

    🚧 need to be update

--- src/middlewares/checkSelfOrAdmin.js
+++ src/middlewares/checkSelfOrAdmin.js
@@ -1,21 +1,17 @@
 const getUserFromToken = require("../utils/getUserFromToken");
 
 const checkSelfOrAdmin = () => {
-	return async (request, reply, done) => {
+	return async (request, reply) => {
 		const token = request.headers.authorization;
 		const { isAuthenticated, role, id } = getUserFromToken(token);
 
 		if (!isAuthenticated) {
-			reply.code(401).send({ error: "Unauthorized" });
-			return;
+			return reply.code(401).send({ error: "Unauthorized" });
 		}
 
 		if (!(id === request.params.id || role === "ADMIN")) {
-			reply.code(403).send({ error: "Permission denied" });
-			return;
+			return reply.code(403).send({ error: "Permission denied" });
 		}
-
-		done();
 	};
 };
 
