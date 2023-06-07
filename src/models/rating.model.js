2749bd2230b31deb6cf3c3484eb0a6edc5c4d23a
Johann six <six.johann@gmail.com>
2023-06-07 16:25:02 +0200

    ✨ add controller model route for rating

---
+++ src/models/rating.model.js
@@ -0,0 +1,137 @@
+const getAllRatingSchema = {
+    response: {
+        200: {
+            type: "array",
+            items: {
+                type: "object",
+                required: ["id", "user", "tutorial", "rating_value", "created_at", "updated_at"],
+                properties: {
+                    id: { type: "integer" },
+                    user: {
+                        type: "object",
+                        required: ["id", "username", "avatar"],
+                        properties: {
+                            id: { type: "integer" },
+                            username: { type: "string", minLength: 3, maxLength: 255 },
+                            avatar: { type: "string", format: "uri", maxLength: 255 },
+                        },
+                        tutorial: {
+                            type: "object",
+                            required: ["id", "title", "created_at"],
+                            properties: {
+                                id: { type: "integer" },
+                                title: { type: "string", minLength: 3, maxLength: 255 },
+                                created_at: { type: "string", format: "date-time" },
+                            },
+                        },
+                        rating_value: { type: "integer" },
+                        created_at: { type: "string", format: "date-time" },
+                        updated_at: { type: "string", format: "date-time" }
+                    },
+                },
+            }
+        }
+    }
+};
+
+const getRatingSchema = {
+    params: {
+        type: "object",
+        required: ["id"],
+        properties: {
+            id: { type: "integer", minimum: 1 },
+        },
+    },
+    response: {
+        200: {
+            type: "object",
+            required: ["id", "user", "tutorial", "rating_value", "created_at", "updated_at"],
+            properties: {
+                id: { type: "integer" },
+                user: {
+                    type: "object",
+                    required: ["id", "username", "avatar"],
+                    properties: {
+                        id: { type: "integer" },
+                        username: { type: "string", minLength: 3, maxLength: 255 },
+                        avatar: { type: "string", format: "uri", maxLength: 255 },
+                    },
+                    tutorial: {
+                        type: "object",
+                        required: ["id", "title", "created_at"],
+                        properties: {
+                            id: { type: "integer" },
+                            title: { type: "string", minLength: 3, maxLength: 255 },
+                            created_at: { type: "string", format: "date-time" },
+                        },
+                    },
+                    rating_value: { type: "integer" },
+                    created_at: { type: "string", format: "date-time" },
+                    updated_at: { type: "string", format: "date-time" }
+                },
+            },
+        },
+    },
+};
+
+const createRatingSchema = {
+    body: {
+        type: "object",
+        required: [ "id_tutorial", "rating_value"],
+        properties: {
+            id_tutorial: { type: "integer" },
+            rating_value: { type: "integer", minimum: 1, maximum: 5 },
+        },
+    },
+    response: {
+        201: {
+            type: "object",
+            required: ["id", "id_user", "id_tutorial", "rating_value", "created_at"],
+            properties: {
+                id: { type: "integer" },
+                id_user: { type: "integer" },
+                id_tutorial: { type: "integer" },
+                rating_value: { type: "integer" },
+                created_at: { type: "string", format: "date-time" },
+            },
+        },
+    },
+};
+
+const updateRatingSchema = {
+    params: {
+        type: "object",
+        required: ["id"],
+        properties: {
+            id: { type: "integer", minimum: 1 },
+        },
+    },
+    body: {
+        type: "object",
+        required: [ "rating_value"],
+        properties: {
+            rating_value: { type: "integer", minimum: 1, maximum: 5 },
+        },
+    },
+    response: {
+        200: {
+            type: "object",
+            required: ["id", "id_user", "id_tutorial", "rating_value", "created_at", "updated_at"],
+            properties: {
+                id: { type: "integer" },
+                id_user: { type: "integer" },
+                id_tutorial: { type: "integer" },
+                rating_value: { type: "integer" },
+                created_at: { type: "string", format: "date-time" },
+                updated_at: { type: "string", format: "date-time" }
+            },
+        },
+    },
+};
+
+module.exports = {
+    getAllRatingSchema,
+    getRatingSchema,
+    createRatingSchema,
+    updateRatingSchema,
+};
---
+++ src/routes/rating.js
@@ -0,0 +1,74 @@
+const RatingController = require("../controllers/ratingController");
+const requireRole = require("../middlewares/requiredRole");
+const checkSelfOrAdmin = require("../middlewares/checkSelfOrAdmin");
+const { getAllRatingSchema, getRatingSchema, createRatingSchema, updateRatingSchema } = require("../models/rating.model");
+
+async function rating(fastify) {
+	const client = fastify.db.client;
+    const ratingController = new RatingController(client);
+	
+
+	fastify.get(
+        "/rating",
+        { schema: getAllRatingSchema, preHandler: requireRole(['ADMIN', 'MODERATOR'], client)},
+        async (request, reply) => {
+            const rating = await ratingController.getAllRating();
+            reply.send(rating);
+        }
+    );
+
+    fastify.get(
+        "/rating/:id",
+        { schema: getRatingSchema, preHandler: requireRole(['ADMIN', 'MODERATOR'],  client)},
+        async (request, reply) => {
+            const rating = await ratingController.getRating(request.params.id);
+            reply.send(rating);
+        }
+    );
+
+    fastify.get(
+        "/rating/tutorial/:id",
+        { schema: getAllRatingSchema, preHandler: requireRole(['ADMIN', 'MODERATOR', 'REDACTOR'],  client)},
+        async (request, reply) => {
+            const rating = await ratingController.getAllRatingByTutorial(request.params.id);
+            reply.send(rating);
+        }
+    );
+
+    fastify.post(
+        "/rating",
+        { schema: createRatingSchema, onRequest: requireRole(['ADMIN', 'MODERATOR', 'REDACTOR', 'USER'],  client)},
+        async (request, reply) => {
+            console.log('Route handler called');
+            const { id_tutorial, rating_value } = request.body;
+            const id_user = request.userId;
+            const rating = await ratingController.createRating(rating_value, id_user, id_tutorial);
+            reply.status(201);
+            reply.send(rating);
+        }
+    );
+
+    fastify.put(
+        "/rating/:id",
+        { schema: updateRatingSchema, preHandler: checkSelfOrAdmin()},
+        async (request, reply) => {
+            const {  rating_value } = request.body;
+            const rating = await ratingController.updateRating(request.params.id, rating_value);
+            reply.status(200);
+            reply.send(rating);
+        }
+    );
+
+    fastify.delete(
+        "/rating/:id", { preHandler: requireRole(['ADMIN', 'MODERATOR'],  client) },async (request, reply) => {
+            const rating = await ratingController.deleteRating(request.params.id);
+            reply.status(204);
+            reply.send(rating);
+        }
+    );
+
+
+
+}
+
+module.exports = rating;
