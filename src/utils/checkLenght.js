c6cda42506126890884c53a236bb18ee2378914b
Johann six <six.johann@gmail.com>
2023-06-09 11:41:47 +0200

    ✨ add check email and check lenght

---
+++ src/utils/checkLenght.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {string} value 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {boolean} return false if the value is not between min and max
+ */
+const checkLenght = (value, min, max) => {
+
+    if (value.length < min || value.length > max) {
+        return false;
+    }
+    return true;
+}
+
+module.exports = checkLenght;
