914e38faad5dd097cb90d21bbb229ae7f289b4a2
Johann six <six.johann@gmail.com>
2023-07-02 15:16:51 +0200

    ✨ add sql

---
+++ db_pf.sql
@@ -0,0 +1,93 @@
+CREATE TABLE "commentary"(
+    "id" SERIAL NOT NULL,
+    "id_tutorial" BIGINT NOT NULL,
+    "id_user" BIGINT NOT NULL,
+    "parent_id" BIGINT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+		FOREIGN KEY (parent_id) REFERENCES commentary(id) ON DELETE CASCADE
+        FOREIGN KEY (id_tutorial) REFERENCES tutorial (id) ON DELETE CASCADE
+);
+ALTER TABLE
+    "commentary" ADD PRIMARY KEY("id");
+CREATE TABLE "rating"(
+    "id" SERIAL NOT NULL,
+    "id_tutorial" BIGINT NOT NULL,
+    "id_user" BIGINT NOT NULL,
+    "rating_value" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+        FOREIGN KEY (id_tutorial) REFERENCES tutorial (id) ON DELETE CASCADE
+);
+ALTER TABLE
+    "rating" ADD PRIMARY KEY("id");
+CREATE TABLE "tutorial"(
+    "id" SERIAL NOT NULL,
+    "id_users" BIGINT NOT NULL,
+    "id_category" BIGINT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT NOT NULL,
+    "view_count" BIGINT NULL,
+    "durate" BIGINT NULL,
+    "draft" BOOLEAN NOT NULL DEFAULT '1',
+    "banned" BOOLEAN NOT NULL DEFAULT '0',
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+);
+ALTER TABLE
+    "tutorial" ADD PRIMARY KEY("id");
+COMMENT
+ON COLUMN
+    "tutorial"."draft" IS 'need moderator to pass to false';
+CREATE TABLE "categorie"(
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "id_category_parent" BIGINT NULL
+		FOREIGN KEY (id_category_parent) REFERENCES categorie(id) ON DELETE CASCADE
+);
+ALTER TABLE
+    "categorie" ADD PRIMARY KEY("id");
+CREATE TABLE "users"(
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(255) NULL,
+    "f_name" VARCHAR(255) NULL,
+    "l_name" VARCHAR(255) NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" VARCHAR(255) CHECK
+        ("role" IN('USER', 'REDACTOR', 'MODERATOR', 'ADMIN')) NOT NULL,
+        "avatar" TEXT NULL,
+        "ban" BOOLEAN NOT NULL DEFAULT '0',
+        "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+        "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+);
+ALTER TABLE
+    "users" ADD PRIMARY KEY("id");
+CREATE TABLE "jwt_tokens"(
+    "id" SERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "token" VARCHAR(255) NOT NULL,
+    "expiration" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+ALTER TABLE
+    "jwt_tokens" ADD PRIMARY KEY("id");
+ALTER TABLE
+    "rating" DROP CONSTRAINT "rating_id_tutorial_foreign", ADD CONSTRAINT "rating_id_tutorial_foreign" FOREIGN KEY("id_tutorial") REFERENCES "tutorial"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+    "commentary" DROP CONSTRAINT "commentary_id_user_foreign", ADD CONSTRAINT "commentary_id_user_foreign" FOREIGN KEY("id_user") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+    "tutorial" ADD CONSTRAINT "tutorial_id_users_foreign" FOREIGN KEY("id_users") REFERENCES "users"("id");
+ALTER TABLE
+    "categorie" ADD CONSTRAINT "categorie_id_category_parent_foreign" FOREIGN KEY("id_category_parent") REFERENCES "categorie"("id");
+ALTER TABLE
+    "tutorial" ADD CONSTRAINT "tutorial_id_category_foreign" FOREIGN KEY("id_category") REFERENCES "categorie"("id");
+ALTER TABLE
+    "commentary" DROP CONSTRAINT "commentary_parent_id_foreign", ADD CONSTRAINT "commentary_parent_id_foreign" FOREIGN KEY("parent_id") REFERENCES "commentary"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+    "commentary" DROP CONSTRAINT "commentary_id_tutorial_foreign", ADD CONSTRAINT "commentary_id_tutorial_foreign" FOREIGN KEY("id_tutorial") REFERENCES "tutorial"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+    "rating" ADD CONSTRAINT "rating_id_user_foreign" FOREIGN KEY("id_user") REFERENCES "users"("id");
+ALTER TABLE
+    "jwt_tokens" DROP CONSTRAINT"jwt_tokens_user_id_foreign",ADD CONSTRAINT "jwt_tokens_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
